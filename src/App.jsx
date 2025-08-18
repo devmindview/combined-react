f8eb239ba2907d0fcee96c1af9156961c8461011
sanin-mn <msanin7003@gmail.com>
2025-08-18 11:41:17 +0530

    added admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import ClientRoute from './client/routes/ClientRoute'
 import ScrolltoTop from './client/components/elements/ScrolltoTop'
 
 function App() {
   return (
     <>
-    <Router>
-      <ScrolltoTop/>
-      <Routes>
-        <Route path='/*' element={<ClientRoute/>}/>
-      </Routes>
-    </Router>
+      <Router>
+        <ScrolltoTop />
+        <Routes>
+          <Route path='/*' element={<ClientRoute />} />
+          <Route
+            path="/admin-access"
+            element={<Navigate to="https://combined.pythonanywhere.com/admin/" replace />}
+          />
+        </Routes>
+      </Router>
     </>
   )
 }
